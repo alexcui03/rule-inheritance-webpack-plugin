13e5ba18798f17d3eb6d2012c82b884d63abae73
Alex Cui <cuizhihui030925@outlook.com>
2026-04-04 17:00:07 +0800

    chore: use google eslint style

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,34 +1,47 @@
-const { defineConfig } = require('eslint/config');
 const js = require('@eslint/js');
+const jsdoc = require('eslint-plugin-jsdoc');
+const googleStyle = require('eslint-config-google');
 const globals = require('globals');
 
-module.exports = defineConfig([
+// These rules are no longer supported, but the Google style package we depend
+// on hasn't been updated in years to remove them, even though they have been
+// removed from the repo. Manually delete them here to avoid breaking linting.
+delete googleStyle.rules['valid-jsdoc'];
+delete googleStyle.rules['require-jsdoc'];
+
+module.exports = [
   {
-		files: [
+    ignores: [
+      'node_modules/*',
+      'coverage/*'
+    ]
+  },
+  js.configs.recommended,
+  jsdoc.configs['flat/recommended'],
+  googleStyle,
+  {
+    rules: {
+      'max-len': ['error', {
+        code: 120,
+        tabWidth: 2
+      }],
+      'comma-dangle': ['error', 'never']
+    }
+  },
+  {
+    files: [
       'index.js',
       'eslint.config.js'
     ],
-    plugins: {
-      js
-    },
-    extends: [
-      'js/recommended'
-    ],
     languageOptions: {
       globals: {
-        ...globals.node,
+        ...globals.node
       }
     }
-	},
+  },
   {
     files: [
       'test/**/*.js'
-    ],
-    plugins: {
-      js
-    },
-    extends: [
-      'js/recommended'
     ],
     languageOptions: {
       globals: {
@@ -37,4 +50,4 @@ module.exports = defineConfig([
       }
     }
   }
-]);
+];
